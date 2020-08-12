d0a03843cc96ed3f3775382105d408d0c991edfb
Ravi Teja <ashinga48@gmail.com>
2020-08-12 21:44:07 +1000

    Update config.js

--- webUI/config.js
+++ webUI/config.js
@@ -2,18 +2,10 @@
  * PM2 monitor servers
  */
 const servers = {
-    'local': [{
-        ip: '127.0.0.1',
-        port: 3001,
-        show: false
-    }, {
-        ip: '127.0.0.1',
-        port: 3002,
-        show: false
-    }],
-    'server1': [{
-        ip: '127.0.0.1',
-        port: 3001,
-        show: false
+    'monitor': [{
+        ip: 'localhost:5000',
+        port: '',
+        name: 'monitor',
+        show: true
     }]
 }
